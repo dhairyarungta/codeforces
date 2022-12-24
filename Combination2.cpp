1e3b0e63b5511a606a75cfdb2888416659c42656
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-24 21:00:25 +0530

    accepted solution

--- Combination2.cpp
+++ Combination2.cpp
@@ -20,35 +20,33 @@ void combination_first(int n , int r){
             
         }
     }
-    // for (int i =0;i<=n;i++){
-    //     for (int j =0;j<=r;j++){
-    //         cout<< ay[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    //     }
     cout<< ay[n][r]<<"\n";
 
 }
 
 void combination_second(int n , int r){
     //space optimised version of combination_first
-
+    if(r>n){
+        cout<< 0;
+        return;
+        }
     r = min(r,n-r);
-    int arr[r+1];
+    int* arr = new int [r+1];
     for (int i =0;i<=r;i++)arr[i]=0;
     arr[0]=1;
 
     for (int i =1;i<=n;i++){
-        for (int j =min(i,r);j>=0;j--){
+        for (int j = min(i,r);j>0;j--){
             arr[j]=arr[j-1]+arr[j];
         }
     }
-
-    cout<< arr[r];
+    cout<< arr[r]<<endl;
 }
 int main(){
     int n , r;
     cin>> n>> r;
     combination_second(n,r);
+    // combination_first(n,r);
+
     return 0;
 }
