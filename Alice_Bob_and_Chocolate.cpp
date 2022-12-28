f5be2a53e73281485c914efe46a7f7d8fb38a603
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-28 23:29:20 +0530

    solution

--- Alice_Bob_and_Chocolate.cpp
+++ Alice_Bob_and_Chocolate.cpp
@@ -15,45 +15,94 @@ void precal(){
 
 }
 
-void solve (){
+// void solve (){
+//     int n ;
+//     cin>>n;
+//     vector<int> a(n);
+//     fr(i,0,n)cin>>a[i];
+
+//     int left = 0, right = n-1;
+//     int alice= 0, bob=0;
+//     int t =0;
+//     int tl = 0,tr=0;
+//     while(left<right){
+//         // int curLeft= a[left];
+//         // int curRight = a[right];
+        
+//         tl++;tr++;
+//         a[left]--;
+//         a[right]--;
+//         if(a[left]==0){
+//             left++;
+//             alice++;
+//             tl=0;
+//         }
+//         if(a[right]==0)
+//         {right--;
+//         bob++;
+//         tr=0;
+//         }
+//     }
+//     if(alice+bob==n){
+//         ;
+//     }
+//     else if(right==left && tl==tr){
+//         alice++;
+//     }
+//     else{
+//         if(tl>tr)alice++;
+//         else bob++;
+//     }
+//     cout<<alice<<" "<<bob<<"\n";
+// }
+
+struct eater
+{
+    int wait;
+    int ate;
+    void init(){
+        ate=0;
+        wait=0;
+        }
+};
+
+
+
+void solve(){
     int n ;
     cin>>n;
-    vector<int> a(n);
-    fr(i,0,n)cin>>a[i];
+    deque<int> dq;
+    struct eater alice, bob;
+    alice.init();
+    bob.init();
+    
+    fr(i,0,n){
+        int k ;
+        cin>>k;
+        dq.push_back(k);
+    }
 
-    int left = 0, right = n-1;
-    int alice= 0, bob=0;
-    int t =0;
-    int tl = 0,tr=0;
-    while(left<right){
-        // int curLeft= a[left];
-        // int curRight = a[right];
-        
-        tl++;tr++;
-        a[left]--;
-        a[right]--;
-        if(a[left]==0){
-            left++;
-            alice++;
-            tl=0;
+    while(!dq.empty()){
+        if(alice.wait!=0)alice.wait--;
+        if(bob.wait!=0)bob.wait--;
+
+        if(alice.wait==0){
+            alice.wait=dq.front();
+            dq.pop_front();
+            alice.ate++;
         }
-        if(a[right]==0)
-        {right--;
-        bob++;
-        tr=0;
+
+        if(bob.wait==0 && dq.size()!=0){
+            bob.ate++;
+            bob.wait=dq.back();
+            dq.pop_back();
         }
+
+
+
     }
-    if(alice+bob==n){
-        ;
-    }
-    else if(right==left && tl==tr){
-        alice++;
-    }
-    else{
-        if(tl>tr)alice++;
-        else bob++;
-    }
-    cout<<alice<<" "<<bob<<"\n";
+
+    cout<<alice.ate<<" "<<bob.ate<<"\n";
 }
 
 int main(){
