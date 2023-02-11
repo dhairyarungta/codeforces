2bbc796568e94a0b9cb3235fe81e1694c1088759
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-11 18:15:13 +0800

    solution

---
+++ little_elephant_and_bits.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i =a;i<b;i++)
+#define loop(x,n) for (int x = 0;x<n;++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+void solve (){
+    string s;
+    cin>>s;
+    string ans = "";
+    bool first = true;
+    for (int i =0;i<s.size();i++){
+        if(s[i]=='0' && first==true){
+            first=false;
+            continue;
+        }
+        else{
+            ans.push_back(s[i]);
+        }
+
+    }
+    if(first==true)
+        ans.pop_back();
+    cout<<ans<<"\n";
+}
+
+int main(){
+    fast_io;
+    cout<<fixed;
+    cout<< setprecision(10);
+    precal();
+    int t =1;
+    // cin>> t ;
+    for (int i =1;i<=t;i++){
+        // cout<< "Case #"<<i<<": ";
+        solve();
+    }
+
+    return 0;
+
+}
