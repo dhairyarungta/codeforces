9defd3005b8d878bf4e50a1ff7ae9fce8545605f
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-11 12:41:09 +0800

    solution

---
+++ maths-basic-problems/array_with_odd_sum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i =a;i<b;i++)
+#define loop(x,n) for (int x = 0;x<n;++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+void solve (){
+    int n ;
+    cin>>n;
+    bool alleven = true;
+    bool allodd = true;
+    for(int i =0;i<n;i++){
+        int temp ;
+        cin>>temp;
+        if(temp%2==1){
+            alleven = false;
+        }
+        else {
+            allodd=false;
+        }
+    }
+    if(alleven==true|| (n%2==0 && allodd==true) ){
+        cout<<"NO"<<"\n";
+        return;
+    }
+    else
+        cout<<"YES"<<"\n";
+        return;
+}
+
+int main(){
+    fast_io;
+    cout<<fixed;
+    cout<< setprecision(10);
+    precal();
+    int t =1;
+    cin>> t ;
+    for (int i =1;i<=t;i++){
+        // cout<< "Case #"<<i<<": ";
+        solve();
+    }
+
+    return 0;
+
+}
