607d01d7a02de785c3d690d9dc4efdebd97abd15
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-15 12:27:01 +0800

    solution

---
+++ carrot_cake.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i =a;i<b;i++)
+#define loop(x,n) for (int x = 0;x<n;++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+void solve (){
+    int n , t, k ,d;
+    cin>>n>>t>>k>>d;
+
+    if(n<=k){
+        cout<<"NO\n";
+        return;
+    }
+
+    for (int i =1;i*k<n;i++){
+        if(d<i*t){
+            cout<<"YES\n";
+            return;
+        }
+    }
+    cout<<"NO\n";
+
+}
+
+int main(){
+    fast_io;
+    cout<<fixed;
+    cout<< setprecision(10);
+    precal();
+    int t =1;
+    // cin>> t ;
+    for (int i =1;i<=t;i++){
+        // cout<< "Case #"<<i<<": ";
+        solve();
+    }
+
+    return 0;
+
+}
