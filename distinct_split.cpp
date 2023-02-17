ceeaf21e49ccd7c077e39c4515c592d646df7170
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-17 12:25:19 +0800

    solution

---
+++ distinct_split.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i =a;i<b;i++)
+#define loop(x,n) for (int x = 0;x<n;++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+void solve (){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    map <char,int> a, b ;
+    for (int i=0;i<n;i++){
+        b[s[i]]++;
+    }
+    int maxindex = 0,valmax =0;
+    for (int i =0;i<n-1;i++){
+        a[s[i]]++; b[s[i]]--;
+        if(b[s[i]]==0){
+            auto iter =  b.find(s[i]);
+            b.erase(iter);
+        }
+        if(b.size()+a.size()>valmax){
+            valmax = a.size()+b.size();
+            maxindex = i;
+        }   
+    }
+    cout<<valmax<<"\n";
+}
+
+int main(){
+    fast_io;
+    cout<<fixed;
+    cout<< setprecision(10);
+    precal();
+    int t =1;
+    cin>> t ;
+    for (int i =1;i<=t;i++){
+        // cout<< "Case #"<<i<<": ";
+        solve();
+    }
+
+    return 0;
+
+}
