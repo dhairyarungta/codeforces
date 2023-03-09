87aa8acbe8905c7d7610e08e183bf5d7bb2b0502
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-09 11:42:59 +0800

    solution

---
+++ creating_the_contest.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i =a;i<b;i++)
+#define loop(x,n) for (int x = 0;x<n;++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+void solve (){
+    ll n;
+    cin>>n;
+    vector<ll>a(n);
+    for (auto &i:a)cin>>i;
+    // ll st= 0, en  =0;
+    // ll count = 0;
+    // ll maxval = INT_MIN;
+
+    // for(;en<n;en++){
+    //     if(a[en]<=2*a[st]){
+    //         maxval = max<ll>(maxval,en-st+1);
+    //     }
+    //     else {
+    //         while(a[en]>2*a[st]){
+    //             st++;
+    //         }
+    //         maxval = max<ll>(maxval,en-st+1);
+    //     }
+    // }
+    ll maxval = 1;
+    ll cur = 1;
+    for (int i =0;i<n-1;i++){
+        if(2*a[i]>=a[i+1]){
+            cur++;
+            maxval = max<ll>(cur,maxval);
+        }
+        else cur = 1;
+
+    }
+
+    cout<<maxval<<"\n";
+
+}
+
+int main(){
+    fast_io;
+    cout<<fixed;
+    cout<< setprecision(10);
+    precal();
+    int t =1;
+    // cin>> t ;
+    for (int i =1;i<=t;i++){
+        // cout<< "Case #"<<i<<": ";
+        solve();
+    }
+
+    return 0;
+
+}
