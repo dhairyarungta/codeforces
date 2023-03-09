4f57443f539c6ccf7e1af9c3b7697df7c6fe1e17
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-09 18:23:54 +0800

    solution flowers

--- new_year_and_treasure_geolocation.cpp
+++ new_year_and_treasure_geolocation.cpp
@@ -10,43 +10,24 @@
 #define all(x) (x).begin(), (x).end()
 
 using namespace std;
-ll k;
-ll dp[1000002]={0};
-ll sum [1000002]={0};
+
 void precal(){
-    dp[0]=1;
-    for (ll i = 1;i<=1000002;i++){
-        if(i>=k){
-            dp[i]= (dp[i-1]%mod+dp[i-k]%mod)%mod;
-        }
-        else {
-            dp[i]=1;
-        }
-    }
-    sum[0]=1;
-    for (ll i =1 ;i<=1000002;i++){
-        sum[i]=(sum[i-1]%mod+dp[i]%mod)%mod;
-    }
-    // for (auto i:dp)cout<<i<<" ";
+
 }
 
 void solve (){
-    ll a, b;
-    cin>>a>>b;
-    ll ans = 0;
-
-    cout<<(sum[b]-sum[a-1]+mod)%mod<<"\n";
+    ll n;
+    cin>>n;
+    
 }
 
 int main(){
     fast_io;
     cout<<fixed;
     cout<< setprecision(10);
-    
-    int t =1;
-    cin>> t ;
-    cin>>k;
     precal();
+    int t =1;
+    // cin>> t ;
     for (int i =1;i<=t;i++){
         // cout<< "Case #"<<i<<": ";
         solve();
